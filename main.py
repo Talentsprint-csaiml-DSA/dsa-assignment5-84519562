f8c32e92ba00e8671ec8e6746863d49c1a82d7a0
Harsha <harsha8339@gmail.com>
2024-11-30 21:35:16 +0530

    2305556

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 def min_coins(coins, target_amount):
-    ""code here""
+    arr=[float('inf')] *(target_amount+1)
+    arr[0]=0
+    for amount in range (1, target_amount+1):
+        for coin in coins:
+            if amount-coin>=0:
+                arr[amount]=min(arr[amount],arr[amount-coin]+1)
+    return arr[target_amount]
+# coins = [1, 4, 6, 9, 14]
+# target_amount = 26
+# print(min_coins(coins, target_amount))
+    
+    
+
+
+    
+
